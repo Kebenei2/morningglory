f8ef4426b9b1e1f8fbcbb71e61e93e3c08393089
Kebenei2 <iankebenei2@gmail.com>
2025-02-04 00:03:21 +0300

    morningglory

---
+++ introduction.py
@@ -0,0 +1,6 @@
+#print is a method used to display a value/outpput
+
+print("Ian")
+print("eMobilis")
+print(39)
+print(100*2)
---
+++ variable.py
@@ -0,0 +1,18 @@
+#variable is a container used to store
+
+number = 10
+firstname = "Ian"
+lastname ="Kebenei"
+language ="python"
+a = 10
+b = 15
+
+print(number)
+print(firstname)
+print(lastname)
+print(firstname,lastname)
+print(language, "is a web programming language")
+print(a + b)
+#The sum of 10 and 15 is 25
+print("The sum of " ,a, "and" ,b, "is"  , a + b)
+
